a5926aab51f14d57447dc66c9f2d228ef1a4a3a5
thebestuser <bestuseralways718@outlook.com>
2020-05-18 02:02:25 +0800

    elasticsearch with nuxt.js framework

---
+++ pages/elastic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Reactive />
+  </div>
+</template>
+
+<script>
+  import Vue from "vue";
+  import Reactive from "./reactive";
+  import ReactiveSearch from "@appbaseio/reactivesearch-vue";
+
+  Vue.use(ReactiveSearch);
+  Vue.config.productionTip = false;
+
+  export default {
+    components: {
+      Reactive
+    }
+  }  
+</script>
